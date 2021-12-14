b794d43c5ab09f7d47baea4cb7a1efd244b9dfe9
Josefin Persson <josefinp94@gmail.com>
2021-12-14 18:14:51 +0100

    skapar databas

---
+++ XmasWish/Database/Database.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmasWish.Database
+{
+    public class Database : DbContext
+    {
+        private const string DatabaseName = "JPXmasWish";
+        private const string ConnString = $"server=(localdb)\\mssqllocaldb;integrated security=true;database={DatabaseName}";
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
